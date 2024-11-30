bf6f8b2add0844c2c6e2892b20e7bc663e35f5db
Viresh Karshan <101453948+vireshkar@users.noreply.github.com>
2024-11-30 22:05:37 +0200

    Add files via upload

--- MariaWholeScript.js
+++ MariaWholeScript.js
@@ -5,26 +5,27 @@ const assert = require('assert');
 async function main() {
     const browser = await playwright.chromium.launch({ headless: false });
     const page = await browser.newPage();
-			
-	await page.goto('https://www.travelstart.co.za/');
-	await page.getByRole('button', { name: 'Yes' }).click();
-	await page.getByLabel('dept_city').fill('Cape Town');
-	await page.getByRole('option', { name: 'Cape Town, International Cape Town, South Africa CPT' }).click();
-	await page.getByRole('option', { name: /London, All Airports London, United Kingdom LON/i }).click();
-	await page.getByLabel('Thursday, December 19,').getByText('19').click();
+            
+    await page.goto('https://www.travelstart.co.za/');
+    await page.getByRole('button', { name: 'Yes' }).click();
+    await page.getByLabel('dept_city').fill('Cape Town');
+    await page.getByRole('option', { name: 'Cape Town, International Cape Town, South Africa CPT' }).click();
+    await page.getByRole('option', { name: /London, All Airports London, United Kingdom LON/i }).click();
+    await page.getByLabel('Thursday, December 19,').getByText('19').click();
 
-	await page.getByLabel('arr_date').click();
-	await page.getByLabel('Thursday, January 2,').nth(1).click();
+    await page.getByLabel('arr_date').click();
+    await page.getByLabel('Thursday, January 2,').nth(1).click();
 
-	await page.getByRole('button', { name: 'Search Flights' }).click();
-	await expect(page.url()).toContain('/results');
-	await page.waitForLoadState();
-	await page.waitForTimeout(3000);
+    await page.getByRole('button', { name: 'Search Flights' }).click();
+    await expect(page.url()).toContain('/results');
+    await page.waitForLoadState('networkidle'); // Wait for network to be idle
+    await page.waitForTimeout(3000); // Additional wait
 
-	const count1 = await page.locator('app-flight-card').count();
-	expect(count1).toBeGreaterThanOrEqual(1);
+    // Check if the flight results are loaded
+    const count1 = await page.locator('app-flight-card').count();
+    expect(count1).toBeGreaterThanOrEqual(1);
 
-    //await browser.close();
+    await browser.close();
 }
 
 main().catch(console.error);
